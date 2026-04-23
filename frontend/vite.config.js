4f3b7df981020b95f6fe56d6ab71d3846a69ba8e
cl3t4p <61201782+cl3t4p@users.noreply.github.com>
2026-04-23 14:11:53 +0200

    update fetch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      "/auth":        "http://localhost:5000",
+      "/events":      "http://localhost:5000",
+      "/users":       "http://localhost:5000",
+      "/calendar":    "http://localhost:5000",
+      "/invitations": "http://localhost:5000",
+    },
+  },
 });
